5260d3358912a9a9a3b0a228c5b8bbffe100aa9b
xFahran <fahran.grdl@gmail.com>
2024-01-29 22:41:10 +0100

    Remplacement des "var" par "let"

--- Script.js
+++ Script.js
@@ -221,9 +221,9 @@ function updateCanvas() {
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
